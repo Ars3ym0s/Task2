6849a985bdc33ec0f7eeea1af82013f565cfa99b
Ars3ym0s <arseymos.elkin@bk.ru>
2023-06-20 23:01:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(Если копируете путь то уберите ковычки)");
+            Console.Write("Введите путь файла: ");
+            string filename = Console.ReadLine();
+            Console.WriteLine();
+
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Файл не найден");
+                return;
+            }
+
+            string fileContents = File.ReadAllText(filename);
+            string[] words = fileContents.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine("Количество слов: {0}", words.Length);
+        }
+    }
+}
